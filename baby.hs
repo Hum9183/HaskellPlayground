a906ce6df09fbe712821a598dedc686f3217665f
Hum9183 <hum9183@gmail.com>
2023-08-06 14:37:54 +0900

    3章勉強ファイルをプッシュ

--- baby.hs
+++ baby.hs
@@ -1,1 +1,72 @@
+doubleMe :: Num a => a -> a
 doubleMe x = x + x
+
+bmiTell :: Double  -> Double -> String
+bmiTell weight height
+    | bmi <= skinny = "You're underweight, you emon you!"
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= fat = "You're fat! Lose weight, fatty!"
+    | otherwise   = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+          (skinny, normal, fat) = (18.5, 25.0, 30.0)
+
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+    | a <= b    = b
+    | otherwise = a
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+    | a == b    = EQ
+    | a <= b    = LT
+    | otherwise = GT
+
+badGreeting :: String
+badGreeting = "Oh! Pfft. It's you."
+
+niceGreeting :: String
+niceGreeting = "Hello! So very nice to see you,"
+
+greet :: String -> String
+greet "Juan" = niceGreeting ++ " Juna!"
+greet "Fernando" = niceGreeting ++ " Fernando!"
+greet name = badGreeting ++ " " ++ name
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstname
+          (l:_) = lastname
+
+calcBmis :: [(Double, Double)] -> [Double]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+    where bmi weight height = weight / height ^ 2
+
+calcBmis' :: [(Double, Double)] -> [Double]
+calcBmis' xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi > 25.0]
+
+
+cylinder :: Double ->  Double -> Double
+cylinder r h =
+    let sideArea = 2 * pi * r * h
+        topArea = pi * pi ^ 2
+    in  sideArea + 2 * topArea
+
+-- head' :: [a] -> a
+-- head' [] = error "No head for empty lists!"
+-- head' (x:_) = x
+head' :: [a] -> a
+head' xs = case xs of [] -> error "No head for empty lists!"
+                      (x:_) -> x
+
+describeList :: [a] -> String
+describeList ls = "The list is "
+                  ++ case ls of [] -> "empty."
+                                [x] -> "a singleton list."
+                                xs -> "a longer list."
+
+describeList' :: [a] -> String
+describeList' ls = "The list is " ++ what ls
+    where what [] = "empty."
+          what [x] = "a singleton list."
+          what xs = "a longer list."
