efd3f276b0cdce61b6ecd49a543a84f0bc37bcc2
Hum9183 <hum9183@gmail.com>
2023-11-24 19:10:09 +0900

    6章：モジュールの勉強進捗を追加

--- modules.hs
+++ modules.hs
@@ -1,5 +1,9 @@
+import Data.Char
 import Data.List
-import qualified Data.Map as M
+import qualified Data.Map as Map
+import Data.Void (vacuous)
+import Graphics.Win32 (wS_BORDER)
+import System.Win32 (COORD(xPos))
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
@@ -9,3 +13,40 @@ wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
 
 isIn :: (Eq a) => [a] -> [a] -> Bool
 needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
+
+encode :: Int -> String -> String
+encode offset msg = map (\c -> chr $ ord c + offset) msg
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
+
+digitSum :: Int -> Int
+digitSum = sum . map digitToInt . show
+
+firstTo40 :: Maybe Int
+firstTo40 = find (\x -> digitSum x == 40) [1..]
+
+firstTo :: Int -> Maybe Int
+firstTo n = find (\x -> digitSum x == n) [1..]
+
+findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey key [] = Nothing
+findKey key ((k,v):xs)
+    | key == k  = Just v
+    | otherwise = findKey key xs
+
+findKey' :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey' key = foldr (\(k, v) acc -> if key == k then Just v else acc) Nothing
+
+phoneBook :: Map.Map String String
+phoneBook = Map.fromList $
+    [("betty", "555-2938")
+    ,("bonnie", "452-2928")
+    ,("pastsy", "493-2928")
+    ,("lucille", "205-2928")
+    ,("wendy", "939-8282")
+    ,("penny", "853-2492")
+    ]
+
+string2digits :: String -> [Int]
+string2digits = map digitToInt . filter isDigit
